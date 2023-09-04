6f5c6ca866f35e7e1ea93168aa55b92a40cb3d30
Umer Kazi <umerkazi@hotmail.com>
2023-09-04 16:15:52 -0400

    .

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+module.exports = {
+    webpack: (config, options) =>
+    {
+        config.module.rules.push({
+            test: /\.pdf$/i,
+            type: 'asset/source'
+        })
+
+        return config
+    },
+}
